005175370689721c5a39a0e3efeacfa4b19d87b6
tabareslorenzo <31422472+tabareslorenzo@users.noreply.github.com>
2019-07-24 23:59:15 -0400

    Add files via upload

---
+++ RayCastShootComplete.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastShootComplete : MonoBehaviour {
+
+	public int gunDamage = 1;											// Set the number of hitpoints that this gun will take away from shot objects with a health script
+	public float fireRate = 0.25f;										// Number in seconds which controls how often the player can fire
+	public float weaponRange = 75f;										// Distance in Unity units over which the player can fire
+	public float hitForce = 99999999999999999999999999999999f;						// Amount of force which will be added to objects with a rigidbody shot by the player
+	public Transform gunEnd;											// Holds a reference to the gun end object, marking the muzzle location of the gun
+	public float movementSpeed = 10f;
+	public float lifetime = 20f;
+	public bool countdown;
+
+	private Camera fpsCam;												// Holds a reference to the first person camera
+	private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);	// WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
+	private AudioSource gunAudio;										// Reference to the audio source which will play our shooting sound effect
+	private LineRenderer laserLine;										// Reference to the LineRenderer component which will display our laserline
+	private float nextFire;												// Float to store the time the player will be allowed to fire again, after firing
+
+	int score;
+	public struct message{
+		public float lifetime;
+		public int score;	
+			}
+	message[] data;
+
+	public message[] ExportData()
+		{
+				return data;
+			}
+
+	void Start ()
+	{
+		// Get and store a reference to our LineRenderer component
+		laserLine = GetComponent<LineRenderer>();
+
+		// Get and store a reference to our AudioSource component
+		gunAudio = GetComponent<AudioSource>();
+
+		// Get and store a reference to our Camera by searching this GameObject and its parents
+		fpsCam = GetComponentInParent<Camera>();
+	}
+
+
+	void Update ()
+	{
+		Physics.gravity = new Vector3(0, 0.1f, 0);
+
+		//for (var touch : Touch in Input.touches)
+		for (int i = 0; i < Input.touchCount; ++i)
+		{
+		// Check if the player has pressed the fire button and if enough time has elapsed since they last fired
+			if ((Input.GetTouch(i).phase == TouchPhase.Began || Input.GetButtonDown("Fire1")) && Time.time > nextFire)
+			{
+			// Update the time when our player can fire next
+				nextFire = Time.time + fireRate;
+
+			// Start our ShotEffect coroutine to turn our laser line on and off
+            		StartCoroutine (ShotEffect());
+
+            // Create a vector at the center of our camera's viewport
+            //Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+				var rayOrigin = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+            // Declare a raycast hit to store information about what our raycast has hit
+            		RaycastHit hit;
+
+
+			// Set the start position for our visual effect for our laser to the position of gunEnd
+				laserLine.SetPosition (0, gunEnd.position);
+
+			// Check if our raycast has hit anything
+
+				if (Physics.Raycast (rayOrigin, out hit, weaponRange))
+				{
+				// Set the end position for our laser line
+					laserLine.SetPosition (1, hit.point);
+
+
+				// Get a reference to a health script attached to the collider we hit
+					ShootableBox health = hit.collider.GetComponent<ShootableBox>();
+
+
+				// If there was a health script attached
+					if (health != null)
+					{
+					// Call the damage function of that script, passing in our gunDamage variable
+						lifetime = health.Damage (gunDamage, lifetime, score);
+					//countdown = true;
+					}
+
+
+				// Check if the object we hit has a rigidbody attached
+					if (hit.rigidbody != null)
+					{
+					// Add force to the rigidbody we hit, in the direction from which it was hit
+						hit.rigidbody.AddForce (-hit.normal * hitForce);
+					//hit.rigidbody.velocity = hit.rigidbody.velocity*2;
+						hit.rigidbody.useGravity = false;
+
+					//transform.Translate(-hit.normal * movementSpeed * Time.deltaTime);
+					//hit.rigidbody.useGravity = true;
+					//transform.Translate(
+					}
+
+				}
+				else
+				{
+				// If we did not hit anything, set the end of the line to a position directly in front of the camera at the distance of weaponRange
+                	//laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+				}
+
+			}
+		}
+
+
+
+	}
+
+
+	private IEnumerator ShotEffect()
+	{
+		// Play the shooting sound effect
+		gunAudio.Play ();
+
+		// Turn on our line renderer
+		laserLine.enabled = true;
+
+		//Wait for .07 seconds
+		yield return shotDuration;
+
+		// Deactivate our line renderer after waiting
+		laserLine.enabled = false;
+	}
+}
---
+++ ShootableBox.cs
@@ -0,0 +1,251 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ShootableBox : MonoBehaviour {
+
+	//The box's current health point total
+	public int currentHealth = 100000;
+	public float force = 2000000f;
+	/*Random rndcolor = new Random();
+	float randonRed = rndcolor.Next(0, 100);
+	float randonGreen = rndcolor.Next(0, 100);
+	float randonBlue = rndcolor.Next(0, 100); */
+	//Color rancolor = new Color();
+	public Rigidbody rb;
+	public Rigidbody rb1;
+	public Vector3 velocity;
+	public bool countdown = false;
+	public float dur =0.1f;
+	public float timer = 0;
+	//public float dur2 =5f;
+	public float timer2 = 0;
+	public float timer3 = 0;
+	public float lifetime;
+	bool check = false;
+	public float count = 0;
+	RayCastShootComplete.message[] mess;
+
+	// Using a method
+	public void ImportData(RayCastShootComplete source)
+	{
+		this.mess = source.ExportData();
+	}
+
+	//Transform.localScale += new Vector3(0.2F, 0.2F, 0.2F)
+	void Start ()
+	{
+		rb = gameObject.GetComponent<Rigidbody>();
+		//rb.velocity.magnitude = rb.velocity.magnitude + 10;
+		rb.velocity = rb.velocity *3;
+	}
+
+	public void finalMoments()
+	{
+
+			Color mycolor = gameObject.GetComponent<Renderer>().material.color;
+			if(count>0)
+			{
+			timer3 += Time.deltaTime;
+			if(timer3>dur*100)
+			{
+				count = 0;
+				timer3 =0;
+			}
+			}
+		if(currentHealth == 0 || check)
+		{
+			if(countdown || check)
+			{
+
+
+				//gameObject.GetComponent<Renderer>().material.color =  Color.black;
+				mycolor = gameObject.GetComponent<Renderer>().material.color;
+				timer += Time.deltaTime;
+				timer2 += Time.deltaTime;
+
+   				if(timer < dur)
+   				{
+   					gameObject.GetComponent<Renderer>().material.color = Color.white;
+   				}
+
+   				if(timer > dur)
+   				{
+   					gameObject.GetComponent<Renderer>().material.color = Color.black;
+
+   				}
+				if(timer >= 2*dur)
+				{
+				timer = 0;
+				}
+
+   				if(timer2 > dur*30)
+   				{
+   					countdown = false;
+					timer2 = 0;
+
+   				}
+
+			}
+		}
+
+	}
+
+	public float Damage(int damageAmount, float lifetime, int score)
+	{
+
+		//gameObject.GetComponent<Renderer>().material.color = Color.black;
+		//subtract damage amount when Damage function is called
+
+		//rb = gameObject.GetComponent<Rigidbody>();
+		rb.velocity = rb.velocity * 5;
+		//rb.velocity = new Vector3(0,1,0);
+		Vector3 x = rb.velocity*-1;
+		if(currentHealth==1)
+		{
+			currentHealth -= damageAmount;
+			countdown = true;
+		}
+
+		finalCountDown(lifetime, countdown);
+		if(currentHealth<=0 && countdown == false){
+				gameObject.SetActive (false);
+			}
+
+
+
+		//var targethit = gameObject;
+
+		//Check if health has fallen below zero
+
+
+		if(countdown)
+		{
+			check = true;
+			countdown = false;
+			currentHealth += damageAmount;
+		}
+
+		var first = Object.Instantiate (gameObject);
+		rb1 = first.GetComponent<Rigidbody>();
+		rb1.velocity = x;
+
+		if(check)
+		{
+			countdown = true;
+			currentHealth -= damageAmount;
+			check = false;
+		}
+
+		Color myColor =  Color.black;
+		Color myColor2 =  Color.white;
+
+
+		Color check1 = first.GetComponent<Renderer> ().material.color;
+		if (check1 == myColor || check1 == myColor2)
+		{
+			first.GetComponent<Renderer> ().material.color = new Color ((float)Random.value, (float)Random.value, (float)Random.value);
+		}
+		if(countdown == false)
+			{
+				//countdown = false;
+				currentHealth -= damageAmount;
+			}
+
+
+
+		finalMoments();
+
+		return lifetime;
+
+
+		//countdown = true;
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Wall02-00m" || col.gameObject.name == "Wall03-00m" || col.gameObject.name == "Wall05-00m")
+		{
+
+			if(count ==0)
+			{
+				//rb.velocity = rb.velocity * 2;
+				count++;
+			//	Debug.Log("sdkjf");
+			}
+			//Destroy(col.gameObject);
+			Color myColor =  Color.black;
+			Color check = GetComponent<Renderer> ().material.color;
+			if (check != myColor && check != Color.white && countdown == false)
+			{
+				gameObject.GetComponent<Renderer> ().material.color = new Color ((float)Random.value, (float)Random.value, (float)Random.value);
+			}
+			col.gameObject.GetComponent<Renderer>().material.color = new Color((float)Random.value, (float)Random.value, (float)Random.value);
+			for (int i = 0; i < 1; i++)
+			{
+				gameObject.transform.Translate (-col.contacts[i].normal * force * Time.deltaTime);
+
+			}
+			/*foreach (ContactPoint contact in col.contacts)
+			{
+				velocity = Vector3.Reflect(velocity, contact.normal);
+				gameObject.transform.Translate(velocity * Time.deltaTime * force);
+			}*/
+		}
+
+
+	}
+
+	void finalCountDown(float lifetime, bool countdown)
+	{
+
+		if (lifetime == 20f)
+		{
+			lifetime = lifetime + Time.time;
+		}
+		else if(countdown)
+		{
+			if(lifetime<Time.time)
+			{
+
+				lifetime = 20f;
+				//countdown = false;
+
+			}
+		}
+	}
+
+	void Update(){
+
+		finalMoments();
+		if(countdown)
+		{
+			finalCountDown(lifetime, countdown);
+		}
+
+	}
+}
+
+public class NewBehaviourScript: MonoBehaviour
+{
+	public float movementSpeed = 10;
+
+	void Update(){
+
+		transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+		//transform.Translate(-hit.normal * movementSpeed * Time.deltaTime);
+
+	}
+}
+/*
+public class DestroyCubes : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "prop_powerCube")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
+*/
